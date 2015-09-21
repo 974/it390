fc68f351ec2fabd74467df7e12a57327928d3b1f
974 <icrawfo89@gmail.com>
2015-09-21 17:40:28 -0400

    added app.js?

---
+++ app.js
@@ -0,0 +1,20 @@
+(function() {
+  var app = angular.module('myApp', [ ]);
+  
+  app.controller('shopHelper', function(){
+  
+  });
+  
+  var list [{
+    name: 'Cookies',
+    price: '3.00',
+    storeName: 'Wegmans',
+    storeAddress: 'address here'
+  },
+  {
+    name: 'Meat',
+    price: '5.00',
+    storeName: 'Giant',
+    storeAddress: 'address here'
+  },];
+})();
