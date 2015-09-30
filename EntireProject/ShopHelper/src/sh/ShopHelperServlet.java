56ed7bb05b70bd7cf7fbe7d5d98586905ff0250c
jcarrubb <jackcarrubba@yahoo.com>
2015-09-29 22:17:22 -0400

    Finalized the Demo
    See http://1-dot-shophelper-1077.appspot.com/

--- EntireProject/ShopHelper/src/sh/ShopHelperServlet.java
+++ EntireProject/ShopHelper/src/sh/ShopHelperServlet.java
@@ -2,47 +2,53 @@ package sh;
 
 import java.io.IOException;
 import java.util.List;
+
 import javax.jdo.PersistenceManager;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
 import jdodb.PMF;
 import jdodb.Store;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
 import java.util.List;
+
 import javax.jdo.Query;
 
 @SuppressWarnings("serial")
 public class ShopHelperServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+			throws IOException, ServletException {
 		Store s = Store.searchForStore("Wegmans");
 		try{
 			if(s == null){
 				LoadStartingData.loadData();
+				System.out.println("Loaded!");
 			}else{
 			}
 		
 		}catch(NullPointerException e){
 			System.out.println("Error!");
 		}
-	    String json = "{";
-	    json += "\"Budget\":\"" + req.getParameter("budget") + "\"";
-	    json += "}";
+	    String jsonB = "{\"Budget\":\"" + req.getParameter("budget") + "\"}";
 	    JSONObject fs = null;
 	    String output = null;
 	    try {
-			fs = new JSONObject(json);
-			 output = fs.getString("Budget");
+	    	fs = new JSONObject(jsonB);
+			output = "Budget: " + fs.getString("Budget");
+		    jsonB = "{\"Type\":\"" + req.getParameter("options") + "\"}";
+		    fs = new JSONObject(jsonB);
+		    output += "\n" + "Type: " +  fs.getString("Type");
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	    resp.setContentType("application/json");
 	    resp.getWriter().println(output);
-	    resp.sendRedirect("/index.html");
 	}
 }
